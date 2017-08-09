c400cc2fada23bc42c532f93b32458c540e83623
Guilherm Tavares <zetavares.rib@gmail.com>
2017-08-08 23:23:13 -0300

    s

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ app = Sanic(__name__)
 async def test(request):
     return json({ "hello": "world" })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1337, workers=4)
+app.run(host="0.0.0.0", port=8000, debug=True)
