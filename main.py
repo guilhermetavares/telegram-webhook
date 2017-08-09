161fc60d71423b8b8670f46a3db02db15a7343a1
Guilherm Tavares <zetavares.rib@gmail.com>
2017-08-08 23:25:46 -0300

    s

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from sanic.response import json
-
+from sanic import Sanic
+from sanic.response import text
 
 app = Sanic(__name__)
 
 @app.route("/")
 async def test(request):
-    return json({ "hello": "world" })
+    return text('Hello world!')
 
 app.run(host="0.0.0.0", port=8000, debug=True)
