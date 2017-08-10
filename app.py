d1c65e69fbe9e6ca8142e44cceefa900414293e0
Guilherm Tavares <zetavares.rib@gmail.com>
2017-08-10 18:34:32 -0300

    balance

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ from sanic.response import text
 app = Sanic('app')
 
 
+def send_balance(token):
+    HEADERS = {
+        'Access-Token': token,
+        'content-type': 'application/json',
+    }
+    url = 'https://api.totalvoice.com.br/composto'
+    return json.loads(requests.post(url, json=data, headers=HEADERS).content.decode())
+
+
 def send(from_, to, token):
     HEADERS = {
         'Access-Token': token,
@@ -37,24 +46,26 @@ async def test(request):
     BASEURL = "https://api.telegram.org/bot{0}/sendMessage".format(TELEGRAM_TOKEN)
 
     token = None
+    try:
+        data = json.loads(request.body.decode())
+    except:
+        return text('PING')
 
-    data = json.loads(request.body.decode())
     message = data.get('message', {}).get('text')
-
-    if message is None:
-        return text('GET')
-
     chat_id = data.get('message', {}).get('chat', {}).get('id')
     post_message = 'Please uses /start, /help or /call!'
     
     if '/start' in message or '/help' in message:
         post_message = '''Make a register in http://www.totalvoice.com.br/, and send:
 /call TOKEN NUMBER_FROM NUMBER_TO'''
+    elif '/balance' in message:
+        action, token = message.split()
+        data = send(from_n, to_n, token)
+        post_message = '{0}'.format(data.get('dados').get('saldo'))
     elif '/call' in message:
         try:
-            message = message.replace('  ', ' ')
-            action, token, from_n, to_n = message.split(' ')
-            post_message = 'call {0} to {1}, wait for the OOOWH AHHHWN WOOOO AAAAHN WAAAAA AAAAAAHN ANN WAAA!'.format(from_n, to_n)
+            action, token, from_n, to_n = message.split()
+            post_message = 'calling to {0}, wait for the OOOWH AHHHWN WOOOO AAAAHN WAAAAA AAAAAAHN ANN WAAA!'.format(to_n)
         except:
             post_message = '''You must use "/call TOKEN NUMBER_FROM NUMBER_TO"'''
 
