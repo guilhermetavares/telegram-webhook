4233e4de879b1195b84b96f30a2b89aa4557b262
Guilherm Tavares <zetavares.rib@gmail.com>
2017-08-09 23:27:22 -0300

    ..

--- app.py
+++ app.py
@@ -1,21 +1,67 @@
 import os
 import requests
+import json
 
 from sanic import Sanic
 from sanic.response import text
 
+
 app = Sanic('app')
 
-@app.route("/", methods=["POST",])
-async def test(request):
-	BASEURL = "https://api.telegram.org/bot"
-	TOKEN = "309262202:AAH2Is4bmUSOqDRzFZFvwEAokUS3iilJ8uA"
-	url = BASEURL + TOKEN + "/sendMessage"
-	print(url)
-	response = requests.post(
-		url,
-		data={ "chat_id": 15343812, "text": 'my message' }).content
-	return text(response)
 
-# app.run(host="localhost", port=8000, debug=True)
+def send(from_, to, token):
+    HEADERS = {
+        'Access-Token': token,
+        'content-type': 'application/json',
+    }
+    url = 'https://api.totalvoice.com.br/composto'
+    data = {
+        'numero_destino': to,
+        'dados': [
+            {
+                'acao': 'audio',
+                'acao_dados': {
+                    'url_audio': 'https://github.com/haskellcamargo/gemidao-do-zap/raw/master/resources/gemidao.mp3'
+                }
+            }
+        ],
+        'bina': from_
+    }
+    return json.loads(requests.post(url, json=data, headers=HEADERS).content)
+
+
+@app.route("/", methods=["POST", "GET"])
+async def test(request):
+    
+    TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+    BASEURL = "https://api.telegram.org/bot{0}/sendMessage".format(TELEGRAM_TOKEN)
+
+    token = None
+
+    data = json.loads(request.body)
+    message = data.get('message', {}).get('text')
+    chat_id = data.get('message', {}).get('chat', {}).get('id')
+    post_message = 'Please uses /start, /help or /call!'
+    
+    if '/start' in message or '/help' in message:
+        post_message = '''Make a register in http://www.totalvoice.com.br/, and send:
+/call TOKEN NUMBER_FROM NUMBER_TO'''
+    elif '/call' in message:
+        try:
+            action, token, from_n, to_n = message.split(' ')
+            post_message = 'call {0} to {1}, wait for the OOOWH AHHHWN WOOOO AAAAHN WAAAAA AAAAAAHN ANN WAAA!'.format(from_n, to_n)
+        except:
+            post_message = '''You must use "/call TOKEN NUMBER_FROM NUMBER_TO"'''
+
+        if token:
+            data = send(from_n, to_n, token)
+
+            if data.get('status') in [405, 403, 400, 401, 500]:
+                post_message = data.get('mensagem')
+
+    response = requests.post(
+        BASEURL,
+        data={ "chat_id": chat_id, "text": post_message}).content
+    return text(response)
+
 app.run(host='0.0.0.0', port=int(os.environ['PORT']))
