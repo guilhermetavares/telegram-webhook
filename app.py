08bd5270c26f7406e9fbd6d7065fc38461e5c77a
Guilherm Tavares <zetavares.rib@gmail.com>
2017-08-09 21:18:45 -0300

    ..

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from sanic import Sanic
 from sanic.response import text
@@ -6,8 +7,15 @@ from sanic.response import text
 app = Sanic('app')
 
 @app.route("/")
-def test(request):
-    return text('Hello world!')
+async def test(request):
+	BASEURL = "https://api.telegram.org/bot"
+	TOKEN = "309262202:AAH2Is4bmUSOqDRzFZFvwEAokUS3iilJ8uA"
+	url = BASEURL + TOKEN + "/sendMessage"
+	print(url)
+	response = requests.post(
+		url,
+		data={ "chat_id": 15343812, "text": 'my message' }).content
+	return text(response)
 
 # app.run(host="localhost", port=8000, debug=True)
 app.run(host='0.0.0.0', port=int(os.environ['PORT']))
